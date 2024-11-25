f0e41c623b642e3b3d786800ec2e07a3c6291100
Sindhu-rajesh <you@example.com>
2024-11-25 12:18:41 +0530

    update

---
+++ react-movie-app/src/App.jsx
@@ -0,0 +1,56 @@
+import { useEffect,useState } from 'react'
+import SearchIcon from "./search.svg"
+import MovieCard from "./MovieCard";
+import './App.css'
+import Navbar from "./components/Navbar"
+
+const API_URL ='http://www.omdbapi.com/?apikey=495e6615';
+function App() {
+  const [movies, setMovies] = useState([])
+  const [searchTerm, setSearchTerm] = useState("");
+  const searchMovies = async (title)=>
+  {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+   setMovies(data.Search);
+  }
+  
+  useEffect(()=>{
+    searchMovies("Joker");
+  },[]);
+  return (
+    <div className="app">
+    <h1>PrimeFlix Movies</h1> <i class="fa-brands fa-suse" ></i>
+    <h3 className='  text-xl font-bold p-4 text-black-600 '>Home</h3>
+    <h3 className='  text-xl font-bold p-4 text-black-600'>Movies</h3>
+    <h3 className='  text-xl font-bold p-4 text-black-600'>Tv Shows</h3>
+    <div className="search">
+      <input placeholder="Search"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)} />
+
+      <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
+    </div>
+
+    {movies?.length > 0 ? (
+      <div className="container">
+        {movies.map((movie) => (
+          <MovieCard movie= {movie} />
+         
+
+        ))}
+      </div>
+    ) : (
+      <div className="empty">
+        <h2 className='text-3xl'>No Movies found</h2>
+      </div>
+    )}
+        <Navbar />
+
+  </div>
+
+  )
+}
+
+export default App
---
+++ react-movie-app/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+ <div>
+      <h1 className='text-1xl font-thin mx-auto text-white'>PrimeFlix</h1><i class="fa-brands fa-suse" ></i>
+      <p className='text-white'>Terms and Privacy Notice Send us feedback Help</p>
+      <hr />
+      <h4 className='text-2xl text-white md:text-center'>1995-2024</h4>
+ </div>
+       
+
+
+  
+   
+   
+  )
+}
+
+export default Navbar
